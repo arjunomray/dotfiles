a7a1d6b01d93cd4efcf18277499104c10fe3b544
arjunomray <arjunomray@gmail.com>
2026-03-29 23:28:41 +0530

    changes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,15 +1,8 @@
 -- lua/plugins/rose-pine.lua
 return {
-  "neanias/everforest-nvim",
-  version = false,
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  -- Optional; default configuration will be used if setup isn't called.
-
+  "rose-pine/neovim",
+  name = "rose-pine",
   config = function()
-    require("everforest").setup({
-      -- Your config here
-      vim.cmd([[colorscheme everforest]]),
-    })
+    vim.cmd("colorscheme rose-pine")
   end,
 }
