0ac2d47d9e2f5a89b1ec0e9e7a268794ae562c4e
arjunomray <arjunomray@gmail.com>
2026-05-20 16:45:13 +0530

    feat: udpates

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -12,8 +12,35 @@ return {
     },
     opts = {
       picker = {
+        actions = {
+          toggle_hidden_with_env = function(picker)
+            picker.opts.hidden = not picker.opts.hidden
+            picker.opts._env_include = picker.opts._env_include
+              or { ".env", ".env.*", "*/.env", "*/.env.*" }
+
+            local include = vim.deepcopy(picker.opts.include or {})
+            include = vim.tbl_filter(function(pattern)
+              return not vim.tbl_contains(picker.opts._env_include, pattern)
+            end, include)
+
+            if picker.opts.hidden then
+              vim.list_extend(include, picker.opts._env_include)
+            end
+
+            picker.opts.include = include
+            picker.list:set_target()
+            picker:find()
+          end,
+        },
         sources = {
           explorer = {
+            win = {
+              list = {
+                keys = {
+                  ["H"] = "toggle_hidden_with_env",
+                },
+              },
+            },
             layout = {
               layout = {
                 position = "right",
